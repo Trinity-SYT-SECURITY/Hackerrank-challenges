a3b68233cbdf54f6d399ff2328e1babf43940073
死老百姓 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2022-12-05 07:19:56 +0800

    Update IntrotoConditionalStatements.py

--- IntrotoConditionalStatements.py
+++ IntrotoConditionalStatements.py
@@ -8,12 +8,13 @@ import sys
 n = int(input().strip())
 
 if n >=1 and n <=100:
-    if n % 2 ==1:
+    if n % 2 == 1:
         print("Weird")
-    elif n >=2 and n <= 5:
+    elif n % 2 == 0 and n >= 2 and n <= 5:
         print("Not Weird")
-    elif n >= 6 and n <= 20:
+    elif n % 2 == 0 and n >= 6 and n <= 20:
         print("Weird")
-    elif n > 20:
+    elif n % 2 == 0 and n > 20:
         print("Not Weird")
+
 
