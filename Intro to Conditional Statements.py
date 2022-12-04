29daa8732b30086e2333f770e6a781322528c1ac
死老百姓 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2022-12-05 00:40:13 +0800

    Create  Intro to Conditional Statements.py

---
+++ Intro to Conditional Statements.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+n = int(input().strip())
+
+if n >=1 and n <=100:
+    if n % 2 ==1:
+        print("Weird")
+    elif n >=2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
+
