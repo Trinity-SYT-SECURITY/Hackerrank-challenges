062ede40442ef061b21613abce744d2c717d497b
死老百姓 심의정 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2022-12-10 23:30:08 +0800

    Add files via upload

---
+++ Dictionaries and Maps.py
@@ -0,0 +1,19 @@
+﻿#d = {key1: value1, key2: value2}
+# print(movie['director'])
+
+n = int(input())
+d = dict()
+
+for _ in range(0, n):
+    name, number = input().split()
+    d[name] = number
+
+while True:
+    try:
+        name = input()
+        if name in d:
+            print('{}={}'.format(name, d[name]))
+        else:
+            print(d.get("", "Not found"))
+    except EOFError:
+        break
