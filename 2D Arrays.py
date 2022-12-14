65a7a994b9c0b9b5a538469c0a22d1cf5533ddb8
死老百姓 심의정 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2022-12-15 04:46:41 +0800

    Add files via upload

---
+++ 2D Arrays.py
@@ -0,0 +1,21 @@
+﻿#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+
+    arr = []
+    sum1 = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    for i in range(4):
+        for j in range(4):
+            sum1.append(arr[i][j]+arr[i][j+1]+arr[i][j+2] +
+                        arr[i+1][j+1]
+                        + arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+            print(arr[i][j])
+    print(max(sum1))
